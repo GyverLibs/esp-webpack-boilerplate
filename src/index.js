feb855b1d6ae97a1b1651a1d22b7dd6212785c34
AlexGyver <alexgyvershow@ya.ru>
2024-06-23 12:20:49 +0300

    upd

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css'
+
+document.addEventListener("DOMContentLoaded", () => {
+    let h = document.createElement('h1');
+    h.innerText = 'Hello, World!';
+    document.body.append(h);
+});
